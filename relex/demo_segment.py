cd0a5aea7ddee33cc4761bf7ea6cde622dbb074a
Cora Lewis <clewis@gsgis.k12.va.us>
2019-10-14 13:05:43 -0400

    added new demo_segmentation and added new sgementation.py

---
+++ relex/demo_segment.py
@@ -0,0 +1,32 @@
+from data import Dataset
+from segment import Segmentation
+
+# path to the dataset
+sample_train = Dataset('../data/sample_train')
+
+'''
+Running instructions: 
+    1. Run each category of the labels separately. Comment out the rest when running 
+    2. Add the entities in the category to the rel_labels list
+    3. To enable the no_relation, add the label name to the no_rel_label list, if not do not pass the list as an argument
+
+'''
+# To extract the problem - treatment relations object
+rel_labels = ['problem', 'test']
+no_rel_label = ['NTeP']
+
+# To extract the problem - treatment relations object
+# rel_labels = ['problem', 'treatment']
+# no_rel_label = ['NTeP']
+
+# To extract the problem - treatment relations object
+# rel_labels = ['problem', 'test']
+# no_rel_label = ['NTeP']
+
+# to extract the segments from the dataset
+seg_sampleTrain = Segmentation(sample_train, rel_labels, no_rel_label )
+
+#print for testing purposes
+sample_sentTrain = seg_sampleTrain.segments['seg_concept1']
+print(sample_sentTrain)
+
