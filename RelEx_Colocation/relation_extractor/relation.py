ecf8568f63497ea6a2775b07f5d49c9da2a41050
swfarnsworth <swfarnsworth@gmail.com>
2019-11-03 09:21:53 -0500

    refactor

--- RelEx_Colocation/relation_extractor/relation.py
+++ RelEx_Colocation/relation_extractor/relation.py
@@ -1,11 +1,14 @@
 #Authour - Samantha Mahendran for RelEx_Colocation
 
-from RelEx_Colocation.utils.ann_to_json import Ann_To_Json
-from RelEx_Colocation.relation_extractor import traversal
-from operator import itemgetter
-import spacy
 import os
 import shutil
+from operator import itemgetter
+
+import spacy
+
+from RelEx_Colocation.relation_extractor import traversal
+from RelEx_Colocation.utils.ann_to_json import Ann_To_Json
+
 
 class Relation:
 
@@ -27,11 +30,11 @@ class Relation:
         # obeject to store the predictions
         self.result = {'relations': [], 'entities': []}
 
-    def find_relations(self, traversal_direction = 'left'):
+    def find_relations(self, traversal_direction='left'):
 
         file_input_path = {}
-        for f in self.ann_files:
 
+        for f in self.ann_files:
             #  get the text file name
             file = os.path.splitext(f)[0]
             t_file = file + self.ext_1
@@ -40,40 +43,41 @@ class Relation:
             with open(os.path.join(self.data_path, f)) as file_object:
                 file_input_path[f] = file_object.read().strip()
 
-                # read files and convert ann files to JSON format
-                ann_to_json = Ann_To_Json(file_input_path[f])
+            # read files and convert ann files to JSON format
+            ann_to_json = Ann_To_Json(file_input_path[f])
 
-                rel_f = os.path.join(self.data_path, t_file)
-                text = open(rel_f, 'r').read()
+            rel_f = os.path.join(self.data_path, t_file)
 
-                #use the spacy model
-                doc = self.nlp(text)
+            with open(rel_f) as file_object:
+                text = file_object.read()
 
-                for id, start, end, label, mention in [ann_to_json.annotations['entities'][key] for key in ann_to_json.annotations['entities']]:
-                    #using spaCy function to find the word using the given span
-                    span = doc.char_span(start, end)
-                    self.result['entities'].append((id, label, start, end, span, f))
+            # use the spacy model
+            doc = self.nlp(text)
 
-                #sort the entities
-                sorted_entities = [ann_to_json.annotations['entities'][key] for key in ann_to_json.annotations['entities']]
-                sorted_entities.sort(key=itemgetter(1))
+            for id, start, end, label, mention in ann_to_json.annotations['entities'].values():
+                #using spaCy function to find the word using the given span
+                span = doc.char_span(start, end)
+                self.result['entities'].append((id, label, start, end, span, f))
 
-                #Calls the traversal method according to the selection of the user. By default, it takes the left only direction
-                #traverse left side only
-                if traversal_direction == 'left':
-                    rel = traversal.traverse_left_only(sorted_entities, self.result, f)
-                # traverse right side only
-                elif traversal_direction == 'right':
-                    rel = traversal.traverse_right_only(sorted_entities, self.result, f)
-                # traverse left side first then right side
-                elif traversal_direction == 'left-right':
-                    rel = traversal.traverse_left_right(sorted_entities, self.result,f)
-                # traverse right side then left side
-                elif traversal_direction == 'right-left':
-                    rel = traversal.traverse_right_left(sorted_entities, self.result,f)
-                #traverse both sides within the sentence boundary
-                elif traversal_direction == 'sentence':
-                    rel = traversal.traverse_within_sentence(sorted_entities, self.result, f, doc)
+            #sort the entities
+            sorted_entities = sorted(ann_to_json.annotations['entities'].values(), key=itemgetter(1))
+
+            # Calls the traversal method according to the selection of the user. By default, it takes the left only direction
+            # traverse left side only
+            if traversal_direction == 'left':
+                rel = traversal.traverse_left_only(sorted_entities, self.result, f)
+            # traverse right side only
+            elif traversal_direction == 'right':
+                rel = traversal.traverse_right_only(sorted_entities, self.result, f)
+            # traverse left side first then right side
+            elif traversal_direction == 'left-right':
+                rel = traversal.traverse_left_right(sorted_entities, self.result,f)
+            # traverse right side then left side
+            elif traversal_direction == 'right-left':
+                rel = traversal.traverse_right_left(sorted_entities, self.result,f)
+            #traverse both sides within the sentence boundary
+            elif traversal_direction == 'sentence':
+                rel = traversal.traverse_within_sentence(sorted_entities, self.result, f, doc)
 
         return rel
 
@@ -98,22 +102,18 @@ class Relation:
             file_entity[f].append((id, label, start, end, str(span).replace("\n", "")))
 
         for file in file_relation:
-            i = 1
-            j = 1
-            output_file = self.prediction_path + '/' + file
-            outfile = open(output_file, 'a')
             ann_rel = ""
             ann_entity = ""
 
-            for label1, label2, arg1, arg2 in file_relation[file]:
+            for i, rel in enumerate(file_relation[file], 1):
+                label1, label2, arg1, arg2 = rel
                 ann_rel += "R%i\t%s-%s Arg1:%s Arg2:%s\n" % (i, label1, label2, arg1, arg2)
-                i += 1
 
-            for id, label, start, end, span in file_entity[file]:
+            for i, ent in enumerate(file_entity[file], 1):
+                id, label, start, end, span = ent
                 ann_entity += "%s\t%s %i %i\t%s\n" % (id, label, start, end, span)
-                j += 1
-
-            outfile.write(ann_entity)
-            outfile.write(ann_rel)
 
 
+            output_file = os.path.join(self.prediction_path, file)
+            with open(output_file, 'a') as outfile:
+                outfile.write(ann_entity + ann_rel)
