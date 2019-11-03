a1cf88ca76ea85966414873c95c19a758a6055e6
swfarnsworth <swfarnsworth@gmail.com>
2019-11-03 10:27:39 -0500

    un-nesting blocks

--- RelEx_Colocation/relation_extractor/traversal.py
+++ RelEx_Colocation/relation_extractor/traversal.py
@@ -9,15 +9,18 @@ def traverse_right_only(sorted_entities, result, f):
     for id, start, end, label, mention in sorted_entities:
         index = sorted_entities.index((id, start, end, label, mention))
 
-        if label != 'Drug':
-            ind_right = index + 1
-            count = 1
-            while ind_right < len_SE:
-                sorted_id, sorted_start, sorted_end, sorted_label, sorted_mention  = sorted_entities[ind_right]
-                if sorted_label == 'Drug':
-                    result['relations'].append((label, sorted_label, id, sorted_id, f))
-                    break
-                ind_right = ind_right + count
+        if label == 'Drug':
+            return result
+
+        ind_right = index + 1
+        count = 1
+        while ind_right < len_SE:
+            sorted_id, sorted_start, sorted_end, sorted_label, sorted_mention  = sorted_entities[ind_right]
+            if sorted_label == 'Drug':
+                result['relations'].append((label, sorted_label, id, sorted_id, f))
+                break
+            ind_right = ind_right + count
+
     return result
 
 # traverse only the left side of the drug mention
@@ -26,15 +29,18 @@ def traverse_left_only(sorted_entities, result, f):
     for id, start, end, label, mention in sorted_entities:
         index = sorted_entities.index((id, start, end, label, mention))
 
-        if label != 'Drug':
-            ind_left = index - 1
-            count = 1
-            while ind_left > -1:
-                sorted_id, sorted_start, sorted_end, sorted_label, sorted_mention  = sorted_entities[ind_left]
-                if sorted_label == 'Drug':
-                    result['relations'].append((label, sorted_label, id, sorted_id, f))
-                    break
-                ind_left = ind_left - count
+        if label == 'Drug':
+            return result
+
+        ind_left = index - 1
+        count = 1
+        while ind_left > -1:
+            sorted_id, sorted_start, sorted_end, sorted_label, sorted_mention  = sorted_entities[ind_left]
+            if sorted_label == 'Drug':
+                result['relations'].append((label, sorted_label, id, sorted_id, f))
+                break
+            ind_left = ind_left - count
+
     return result
 
 #traverse right first then left of the drug mention
@@ -44,23 +50,25 @@ def traverse_right_left(sorted_entities, result, f):
     for id, start, end, label , mention in sorted_entities:
         index = sorted_entities.index((id, start, end, label, mention))
 
-        if label != 'Drug':
-            ind_left = index - 1
-            ind_right = index + 1
-            count = 1
+        if label == 'Drug':
+            return result
 
-            while ind_left > -1 and ind_right < len_SE:
-                right_id, right_start, right_end, right_label, right_mention = sorted_entities[ind_right]
-                if right_label == 'Drug':
-                    result['relations'].append((label, right_label, id, right_id, f))
-                    break
-                ind_right = ind_right + count
+        ind_left = index - 1
+        ind_right = index + 1
+        count = 1
 
-                left_id, left_start, left_end, left_label, left_mention = sorted_entities[ind_left]
-                if left_label == 'Drug':
-                    result['relations'].append((label, left_label, id, left_id, f))
-                    break
-                ind_left = ind_left - count
+        while ind_left > -1 and ind_right < len_SE:
+            right_id, right_start, right_end, right_label, right_mention = sorted_entities[ind_right]
+            if right_label == 'Drug':
+                result['relations'].append((label, right_label, id, right_id, f))
+                break
+            ind_right = ind_right + count
+
+            left_id, left_start, left_end, left_label, left_mention = sorted_entities[ind_left]
+            if left_label == 'Drug':
+                result['relations'].append((label, left_label, id, left_id, f))
+                break
+            ind_left = ind_left - count
 
     return result
 
@@ -71,23 +79,25 @@ def traverse_left_right(sorted_entities, result, f):
     for id, start, end, label, mention in sorted_entities:
         index = sorted_entities.index((id, start, end, label, mention))
 
-        if label != 'Drug':
-            ind_left = index - 1
-            ind_right = index + 1
-            count = 1
+        if label == 'Drug':
+            return result
 
-            while ind_left > -1 and ind_right < len_SE:
-                left_id, left_start, left_end, left_label, left_mention = sorted_entities[ind_left]
-                if left_label == 'Drug':
-                    result['relations'].append((label, left_label, id, left_id, f))
-                    break
-                ind_left = ind_left - count
+        ind_left = index - 1
+        ind_right = index + 1
+        count = 1
 
-                right_id, right_start, right_end, right_label, right_mention = sorted_entities[ind_right]
-                if right_label == 'Drug':
-                    result['relations'].append((label, right_label, id, right_id, f))
-                    break
-                ind_right = ind_right + count
+        while ind_left > -1 and ind_right < len_SE:
+            left_id, left_start, left_end, left_label, left_mention = sorted_entities[ind_left]
+            if left_label == 'Drug':
+                result['relations'].append((label, left_label, id, left_id, f))
+                break
+            ind_left = ind_left - count
+
+            right_id, right_start, right_end, right_label, right_mention = sorted_entities[ind_right]
+            if right_label == 'Drug':
+                result['relations'].append((label, right_label, id, right_id, f))
+                break
+            ind_right = ind_right + count
 
     return result
 
@@ -98,43 +108,46 @@ def traverse_within_sentence(sorted_entities, result, f,doc):
     for id, start, end, label, mention in sorted_entities:
         index = sorted_entities.index((id, start, end, label, mention))
 
-        if label != 'Drug':
-            span = doc.char_span(start, end)
-            #finds the correct span if the given span doesn't match the correct span
-            new_span = alternative_span.find_alternative_span(start, end, span, doc)
+        if label == 'Drug':
+            return result
 
-            if new_span:
-                span_sent = str(new_span.sent).split()
-                ind_left = index - 1
-                count_left = 1
+        span = doc.char_span(start, end)
+        #finds the correct span if the given span doesn't match the correct span
+        new_span = alternative_span.find_alternative_span(start, end, span, doc)
 
-                while ind_left > -1:
-                    left_id, left_start, left_end, left_label, left_mention = sorted_entities[ind_left]
-                    span_left = doc.char_span(left_start, left_end)
-                    new_span_left = alternative_span.find_alternative_span(start, end, span_left, doc)
+        if new_span:
+            span_sent = str(new_span.sent).split()
+            ind_left = index - 1
+            count_left = 1
 
-                    if left_label == 'Drug':
-                        if any(str(new_span_left[0]) in s for s in span_sent):
-                            result['relations'].append((label, left_label, id, left_id, f))
-                        else:
-                            break
-                    ind_left = ind_left - count_left
+            while ind_left > -1:
+                left_id, left_start, left_end, left_label, left_mention = sorted_entities[ind_left]
+                span_left = doc.char_span(left_start, left_end)
+                new_span_left = alternative_span.find_alternative_span(start, end, span_left, doc)
 
-                ind_right = index + 1
-                count_right = 1
+                if left_label == 'Drug':
+                    if any(str(new_span_left[0]) in s for s in span_sent):
+                        result['relations'].append((label, left_label, id, left_id, f))
+                    else:
+                        break
+                ind_left = ind_left - count_left
 
-                while ind_right < len_SE:
-                    right_id, right_start, right_end, right_label, right_mention = sorted_entities[ind_right]
-                    span_right = doc.char_span(right_start, right_end)
-                    new_span_right = alternative_span.find_alternative_span(start, end, span_right, doc)
+            ind_right = index + 1
+            count_right = 1
 
-                    if right_label == 'Drug':
-                        if any(str(new_span_right[0]) in s for s in span_sent):
-                            result['relations'].append((label, right_label, id, right_id, f))
-                        else:
-                            break
+            while ind_right < len_SE:
+                right_id, right_start, right_end, right_label, right_mention = sorted_entities[ind_right]
+                span_right = doc.char_span(right_start, right_end)
+                new_span_right = alternative_span.find_alternative_span(start, end, span_right, doc)
 
-                    ind_right = ind_right + count_right
+                if right_label == 'Drug':
+                    if any(str(new_span_right[0]) in s for s in span_sent):
+                        result['relations'].append((label, right_label, id, right_id, f))
+                    else:
+                        break
+
+                ind_right = ind_right + count_right
+
     return result
 
 
