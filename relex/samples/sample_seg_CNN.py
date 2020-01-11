cbcc399023bb875f426091366aaed0eaa84e6b20
SamMahen <samanthamahendran6@gmail.com>
2020-01-10 20:34:52 -0500

    Adding samples and guide

---
+++ relex/samples/sample_seg_CNN.py
@@ -0,0 +1,16 @@
+from RelEx_NN.model import Model
+from RelEx_NN.embeddings import Embeddings
+from RelEx_NN.cnn import Segment_CNN
+from segment import Set_Connection
+
+# Use pre-trained word embeddings
+embedding_path = "../word_embeddings/glove.6B.200d.txt"
+
+# path to sentence and label CSV files
+data = Set_Connection(CSV=True, sentences='../data/segments/sentence_train', labels='../data/segments/labels_train',preceding_segs='../data/segments/preceding_seg', concept1_segs='../data/segments/concept1_seg',middle_segs='../data/segments/middle_seg',concept2_segs='../data/segments/concept2_seg', succeeding_segs='../data/segments/succeeding_seg' ).data_object
+
+model = Model(data)
+embedding=Embeddings(embedding_path, model)
+
+# train CNN model
+seg_cnn = Segment_CNN(model, embedding, True)
