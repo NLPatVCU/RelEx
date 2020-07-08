d868c0d26c47f67105d1ddb3e15d3151594b13c7
SamMahen <samanthamahendran6@gmail.com>
2020-07-08 18:50:05 -0400

    corrected mismatch

--- relex/segment/set_connection.py
+++ relex/segment/set_connection.py
@@ -10,19 +10,26 @@ from csv import reader
 class Set_Connection:
     def __init__(self, sentence_only = False, sentences=None, labels=None, preceding_segs=None, concept1_segs=None,
                  middle_segs=None, concept2_segs=None, succeeding_segs=None, track=None, dataset=None,
-                 rel_labels=None, no_labels=None, CSV=True, test=False, parallelize= False, no_of_cores = 64):
+                 rel_labels=None, no_labels=None, CSV=True, test=False, parallelize= False, no_of_cores = 64, predictions_folder = None, write_Entites = False):
         """
-        Creates object based on data either from a dataset folder or a set of CSVs
-        :param dataset: path to dataset
-        :param rel_labels: list of options for relationship labels
-        :param no_labels: list with label for when there is no relationship
-        :param CSV: flag: false if creating object from dataset, true if using CSVs
+        Creates data objects directly from the dataset folder and call for segmentation or take in segments (a set of CSVs)
+        :type write_Entites: write entities and predictions to file
+        :param sentence_only: flag when we need to consider segments for sentence CNN only
         :param sentences: path to sentences CSV
         :param labels: path to labels CSV
         :param preceding_segs: path to preceding segments CSV
         :param concept1_segs: path to concept1 segments CSV
         :param middle_segs: path to middle segements CSV
         :param succeeding_segs: path to succeeding segments CSV
+        :param track: path to track information (file, first entity, second entity)
+        :param dataset: path to dataset
+        :param rel_labels: list of entities that create the relations
+        :param no_labels: name the label when entities that do not have relations in a sentence are considered
+        :param CSV: flag to decide to read from the CSVs directly
+        :param test: flag to run test-segmentation options
+        :param parallelize: flag to parallelize the segmentation
+        :param no_of_cores: no of cores to run the parallelized segmentation
+        :param predictions_folder: path to predictions (output) folder
         """
         self.CSV = CSV
         self.sentence_only = sentence_only
@@ -41,26 +48,27 @@ class Set_Connection:
             self.data_object = self.get_data_object
 
         else:
-            # if there are no CSVs, runs the Segmentation module to get object
+            # if there are no CSVs, runs the Segmentation module to get the segmentation object
             self.dataset = Dataset(dataset)
             self.rel_labels = rel_labels
             self.no_labels = no_labels
             if self.parallelize:
-                self.data_object = Segmentation(self.dataset, self.rel_labels, self.no_labels, test=self.test, parallelize = True, no_of_cores=no_of_cores ).segments
+                self.data_object = Segmentation(self.dataset, self.rel_labels, self.no_labels, test=self.test, parallelize = True, no_of_cores=no_of_cores, predictions_folder = predictions_folder,write_Entites = write_Entites ).segments
             else:
-                self.data_object = Segmentation(self.dataset, self.rel_labels, self.no_labels, test=self.test ).segments
+                self.data_object = Segmentation(self.dataset, self.rel_labels, self.no_labels, test=self.test, predictions_folder = predictions_folder,write_Entites = write_Entites).segments
 
     @property
     def get_data_object(self):
         """
-        creates object from CSVs
+        creates segmentation object from CSVs
         """
         obj = {}
 
         # gets segments, labels, and sentences from file
         train_data = file.read_from_file(self.sentences)
         train_labels = file.read_from_file(self.labels)
-        track_list = file.read_from_file(self.track, read_as_int=True)
+        track_list = file.read_from_file(self.track)
+        # track_list = file.read_from_file(self.track, read_as_int=True)
 
         if not self.sentence_only:
             train_preceding = file.read_from_file(self.preceding_segs)
