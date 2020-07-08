f34e564281d83d29df0a1fc0fa029f676adba1e8
SamMahen <samanthamahendran6@gmail.com>
2020-07-08 18:39:47 -0400

    corrected mismatch

--- relex/train_test.py
+++ relex/train_test.py
@@ -8,44 +8,55 @@ from segment import Segmentation
 import sys
 import ast
 
-def segment (train, test, labels, no_rel=None):
-    print("Performing segment TEST---------------------------------------------------------------------")
-    seg_test = Set_Connection(CSV=False, dataset=test, rel_labels=labels, test=True, parallelize= True, no_of_cores = 128).data_object
-    print("Performing segment TRAIN---------------------------------------------------------------------")
+def segment (train, test, entites, no_rel=None, parallelize= False, no_of_cores = 64, predictions_folder = None):
+    """
+    Perform segmentation for the training and testing data
+    :param write_Entites:
+    :param parallelize:
+    :param no_of_cores:
+    :param train: path to train data
+    :param test: path to test data
+    :param entites: list of entities that create the relations
+    :param no_rel: name the label when entities that do not have relations in a sentence are considered
+    :param predictions_folder: path to predictions (output) folder
+    :return: segments of train and test data
+    """
     if no_rel:
-        seg_train = Set_Connection(CSV=False, dataset=train, rel_labels=labels, no_labels=no_rel, parallelize= True, no_of_cores = 128).data_object
+        seg_train = Set_Connection(CSV=False, dataset=train, rel_labels=entites, no_labels=no_rel, write_Entites = False,
+                                   parallelize= parallelize, no_of_cores = no_of_cores, predictions_folder = predictions_folder).data_object
     else:
-        seg_train = Set_Connection(CSV=False, dataset=train, rel_labels=labels, parallelize= True, no_of_cores = 128).data_object
+        seg_train = Set_Connection(CSV=False, dataset=train, rel_labels=entites, write_Entites = False,
+                                   parallelize= parallelize, no_of_cores = no_of_cores, predictions_folder = predictions_folder).data_object
+    seg_test = Set_Connection(CSV=False, dataset=test, rel_labels=entites, test= True, parallelize=True, write_Entites = True,
+                              no_of_cores=64, predictions_folder = predictions_folder).data_object
 
     return seg_train, seg_test
 
-def run_CNN_model (model,seg_train, seg_test):
-
-    embed = model[0]
-    embed_dim = int(model[1])
-    cnn_model = model[2]
-    no_rel = ast.literal_eval(model[3])
-    predictions_path = model[4]
-
-    if embed == 'mimic':
-        if embed_dim == 200:
-            embedding_path = "../word_embeddings/mimic3_d200.txt"
-        else:
-            embedding_path = "../word_embeddings/mimic3_d300.txt"
-    elif embed == 'glove':
-        if embed_dim== 200:
-            embedding_path = "../word_embeddings/glove.6B.200d.txt"
-        else:
-            embedding_path = "../word_embeddings/glove.6B.300d.txt"
-    else:
-        if embed_dim == 200:
-            embedding_path = "../word_embeddings/patent_w2v.txt"
-
+def run_CNN_model (seg_train, seg_test, embedding_path, embed_dim, cnn_model, write_predictions = False, write_No_rel = False, initial_predictions = None, final_predictions=None):
+    """
+    Call CNN models
+    :param seg_test: test segments
+    :param cnn_model: choose the model
+    :param seg_train: train segments
+    :type write_Predictions: write entities and predictions to file
+    :param initial_predictions: folder to save the initial relation predictions
+    :param final_predictions: folder to save the final relation predictions
+    :param write_No_rel: Write the no-relation predictions back to files
+    :return: None
+    """
     if cnn_model == 'segment':
-        model = Model(data_object=seg_train, data_object_test=seg_test, segment=True, test=True,
-                      write_Predictions=True)
+        model = Model(data_object=seg_train, data_object_test=seg_test, segment=True, test=True, write_Predictions=write_predictions)
         embedding = Embeddings(embedding_path, model, embedding_dim=embed_dim)
-        if no_rel is not None and predictions_path is not None:
-            seg_cnn = Segment_CNN(model, embedding, final_predictions= predictions_path, No_Rel=no_rel)
-        else:
-            seg_cnn = Segment_CNN(model, embedding)
+        seg_cnn = Segment_CNN(model, embedding, initial_predictions = initial_predictions,
+                              final_predictions= final_predictions, write_No_rel=write_No_rel)
+
+    elif cnn_model == 'sentence':
+        model = Model(data_object=seg_train, data_object_test=seg_test, test=True, write_Predictions=write_predictions)
+        embedding = Embeddings(embedding_path, model, embedding_dim=embed_dim)
+        single_sent_cnn = Sentence_CNN(model, embedding,initial_predictions=initial_predictions,
+                              final_predictions=final_predictions, write_No_rel=write_No_rel)
+    else:
+        #multilabel sentence-CNN does not have the option to write the predictions back
+        model = Model(data_object=seg_train, data_object_test=seg_test, test=True, multilabel=True)
+        embedding = Embeddings(embedding_path, model, embedding_dim=embed_dim)
+        multi_sent_cnn = Sentence_CNN(model, embedding)
